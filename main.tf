a107be8e3d3a358d55b9a3e3fae4bb123357b5af
malliendluri <malliendluri@gmail.com>
2024-01-20 19:42:26 +0530

    added maintf file

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider"aws"{
+    region = "us-east-1"
+}
+resource"aws_instance" "Example"{
+    ami           = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    subnet_id     = "subnet-09a7a5b0346992524"
+    key_name      = "docker"
+    tags = "terraform1"
+    
+}
